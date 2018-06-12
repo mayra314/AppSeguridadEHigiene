2f7053e7d52a67c2d38d0712f7faeaf857a96fe1
arturo bravo <artbmz005@gmail.com>
2018-06-12 09:44:49 -0500

    2 pantallas

--- app/src/main/java/com/example/miran/appseguridadehigiene/LoginFragment.java
+++ app/src/main/java/com/example/miran/appseguridadehigiene/LoginFragment.java
@@ -6,13 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LoginFragment extends Fragment {
-
+private Button loginFragment;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -23,7 +25,22 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        loginFragment = view.findViewById(R.id.bt_loginFragment);
+        loginFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction()
+                        .replace(android.R.id.content,new HSEFragment())
+                        .addToBackStack(null).commit();
+            }
+        });
+    return view;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        getActivity().setTitle("Seguridad e higiene");
+    }
 }
