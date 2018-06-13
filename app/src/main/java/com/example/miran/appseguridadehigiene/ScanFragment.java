93acbf9fcd325e893e478f212d8338cb1424d90b
Unknown <miranda31416@gmail.com>
2018-06-13 12:06:50 -0500

    Scan

--- app/src/main/java/com/example/miran/appseguridadehigiene/ScanFragment.java
+++ app/src/main/java/com/example/miran/appseguridadehigiene/ScanFragment.java
@@ -31,7 +31,13 @@ public class ScanFragment extends Fragment {
 
     public ScanFragment() {
         // Required empty public constructor
+
+
+
     }
+
+
+
 
     /**
      * Use this factory method to create a new instance of
@@ -49,6 +55,9 @@ public class ScanFragment extends Fragment {
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
+
+
+
     }
 
     @Override
@@ -91,18 +100,13 @@ public class ScanFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+
+
 }
