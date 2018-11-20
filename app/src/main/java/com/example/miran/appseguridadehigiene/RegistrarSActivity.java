911004dbf4db579284fa45357b4ea113db878a58
Unknown <miranda31416@gmail.com>
2018-11-20 10:05:22 -0600

    Registro_Sancion

---
+++ app/src/main/java/com/example/miran/appseguridadehigiene/RegistrarSActivity.java
@@ -0,0 +1,13 @@
+package com.example.miran.appseguridadehigiene;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistrarSActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_s);
+    }
+}
