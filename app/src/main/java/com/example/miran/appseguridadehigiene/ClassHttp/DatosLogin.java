17b44be4e1efd321642a063d7d3712cf3b889024
Unknown <miranda31416@gmail.com>
2018-11-23 09:36:07 -0600

    commit

--- app/src/main/java/com/example/miran/appseguridadehigiene/ClassHttp/DatosLogin.java
+++ app/src/main/java/com/example/miran/appseguridadehigiene/ClassHttp/DatosLogin.java
@@ -1,4 +1,16 @@
 package com.example.miran.appseguridadehigiene.ClassHttp;
 
+import java.util.List;
+
 public class DatosLogin {
+
+    private static final String Logeo="DatosLogin";
+    private List<ResponseService>objetoListLogin;
+
+
+
+
+
+
+
 }
